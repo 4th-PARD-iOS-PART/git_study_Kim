766f580d2d9e6b6de9a82fe33660b5d36271b66f
Blackcode2 <doky03115@gmail.com>
2024-11-06 21:48:58 +0900

    first commit

---
+++ git_study/git_study/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  git_study
+//
+//  Created by 김도경 on 2024/11/06.
+//
+
+// ViewController.swift
+import UIKit
+
+class ViewController: UIViewController {
+    private let mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "안녕하세요"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(mainLabel)
+        
+        NSLayoutConstraint.activate([
+            mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
